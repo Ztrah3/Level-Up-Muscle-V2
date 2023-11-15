164b808053885ae548bcfe1eb60bc789f332facd
Zane <zane1040@gamil.com>
2023-11-14 20:51:17 -0600

    adding comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
 
+// Defining the routes for the application
 const routes: Routes = [
+  // Default route that loads the HomePageComponent
   { path: '', component: HomePageComponent },
+  // Route that loads the WorkoutsComponent
   { path: 'workouts', component: WorkoutsComponent }
 ];
 
+// @NgModule decorator is used to define a module. 
+// defines the routing module for the application
 @NgModule({
+  // The RouterModule.forRoot method is called with the routes defined above. 
+  // This method returns a module that contains all the directives and services needed for routing
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/product-table/product-table.component.ts
+++ src/app/product-table/product-table.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-table.component.css']
 })
 export class ProductTableComponent {
-  products = [
-    {name: 'Workout Plan 1', description: 'Description 1', price: '\$10'},
-    {name: 'Workout Plan 2', description: 'Description 2', price: '\$20'},
-    {name: 'Workout Plan 3', description: 'Description 3', price: '\$30'},
-    // Add more products as needed
-  ];
+
 }
